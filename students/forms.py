8a975b197fe9e67df0a476b41b916f8342721d81
babulswain10101 <babulswain10101@gmail.com>
2026-08-13 13:26:40 +0530

    Update college management system

--- students/forms.py
+++ students/forms.py
@@ -1,12 +1,10 @@
 from django import forms
-
 from .models import Student
 
 
 class StudentForm(forms.ModelForm):
 
     class Meta:
-
         model = Student
 
         fields = [
@@ -23,73 +21,60 @@ class StudentForm(forms.ModelForm):
         ]
 
         widgets = {
+            "first_name": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter first name",
+            }),
 
-            "first_name": forms.TextInput(
+            "last_name": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter last name",
+            }),
+
+            "roll_no": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter roll number",
+            }),
+
+            "email": forms.EmailInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter email",
+            }),
+
+            "phone": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter phone number",
+            }),
+
+            "gender": forms.Select(
                 attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter first name",
-                }
+                    "class": "form-select",
+                },
+                choices=[
+                    ("", "Select Gender"),
+                    ("Male", "Male"),
+                    ("Female", "Female"),
+                    ("Other", "Other"),
+                ],
             ),
 
-            "last_name": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter last name",
-                }
-            ),
+            "department": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter department",
+            }),
 
-            "roll_no": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter roll number",
-                }
-            ),
+            "course": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter course",
+            }),
 
-            "email": forms.EmailInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter email",
-                }
-            ),
+            "year": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Enter year",
+            }),
 
-            "phone": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter phone number",
-                }
-            ),
-
-            "gender": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Male / Female",
-                }
-            ),
-
-            "department": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter department",
-                }
-            ),
-
-            "course": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter course",
-                }
-            ),
-
-            "year": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter year",
-                }
-            ),
-
-            "photo": forms.ClearableFileInput(
-                attrs={
-                    "class": "form-control",
-                }
-            ),
+            "photo": forms.ClearableFileInput(attrs={
+                "class": "form-control",
+                "accept": "image/jpeg,image/png,image/jpg",
+            }),
         }
